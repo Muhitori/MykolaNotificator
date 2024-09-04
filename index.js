1e88bbc5487e02ad8cdb1356e73b0614973a756c
MykolaTurevych <mykola@serbglobal.com>
2024-09-04 23:10:03 +0300

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+
+const { TOKEN, PERSONAL_USER_ID, SERVER_ID } = process.env;
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.DirectMessages,
+	],
+});
+
+client.on("messageCreate", async (message) => {
+	if (
+		message?.guild &&
+		message?.guild?.id === SERVER_ID &&
+		!message.author.bot
+	) {
+		try {
+			const user = await client.users.fetch(PERSONAL_USER_ID);
+			user.send(
+				`New message in #${message?.channel?.name} (Server: ${message?.guild?.name}): ${message?.content}`
+			);
+		} catch (error) {
+			console.error("Error sending message:", error);
+		}
+	}
+});
+
+client.once(Events.ClientReady, () => {
+	console.log(`Logged in as ${client.user.tag}`);
+	console.log("server", SERVER_ID);
+	console.log("receiver", PERSONAL_USER_ID);
+});
+
+client.login(TOKEN);
