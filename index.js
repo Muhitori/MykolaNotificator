9ca4642fedc37a8c021425e71523fc3c08bac0b6
MykolaTurevych <mykola@serbglobal.com>
2024-09-05 23:06:58 +0300

    updated bot logic

--- index.js
+++ index.js
@@ -9,10 +9,32 @@ const client = new Client({
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.MessageContent,
 		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.GuildPresences,
 	],
 });
 
-client.on("messageCreate", async (message) => {
+client.once(Events.ClientReady, () => {
+	console.log(`Logged in as ${client.user.tag}`);
+	console.log("server", SERVER_ID);
+	console.log("receiver", PERSONAL_USER_ID);
+	client.user.setPresence({
+		activities: [{ name: "Watching for mentions!" }],
+		status: "online",
+	});
+});
+
+client.on("raw", async (packet) => {
+	if (packet.t === "MESSAGE_CREATE") {
+		const { channel_id, author, content } = packet.d;
+
+		const channel = await client.channels.fetch(channel_id);
+		const message = await channel.messages.fetch(packet.d.id);
+
+		client.emit("MyMessageEvent", message);
+	}
+});
+
+client.on("MyMessageEvent", async (message) => {
 	if (
 		message?.guild &&
 		message?.guild?.id === SERVER_ID &&
@@ -28,12 +50,15 @@ client.on("messageCreate", async (message) => {
 			console.error("Error sending message:", error);
 		}
 	}
-});
 
-client.once(Events.ClientReady, () => {
-	console.log(`Logged in as ${client.user.tag}`);
-	console.log("server", SERVER_ID);
-	console.log("receiver", PERSONAL_USER_ID);
+	if (!message.guild && !message.author.bot) {
+		try {
+			const user = await client.users.fetch(PERSONAL_USER_ID);
+			user.send(`${message.author.username} to me: ${message.content}`);
+		} catch (error) {
+			console.error("Error sending bot's own DM:", error);
+		}
+	}
 });
 
 client.login(TOKEN);
